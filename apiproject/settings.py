fd47a7fed76e6e04b9d0d2b7a4b46902931d49b7
TayeburAH <tayebur687@gmail.com>
2021-12-01 22:31:36 +0600

    Deploy 2

--- apiproject/settings.py
+++ apiproject/settings.py
@@ -100,12 +100,24 @@ WSGI_APPLICATION = 'apiproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['name'],
+        'USER': os.environ['user'],
+        'PASSWORD': os.environ['password'],
+        'HOST': os.environ['host'],
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -267,6 +279,8 @@ REST_USE_JWT = True
 
 django_heroku.settings(locals())
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 # CLOUDINARY_STORAGE = {
 #     'CLOUD_NAME': 'tayebur-cloud',
 #     'API_KEY': '178946229634178',
